816cad1dfa78baa2e8b732c8dcab89d7ed46a28e
Paul Milne <hallhill@gmail.com>
2016-11-28 16:11:31 +0000

    section 6 completed

--- day_1_hw_js.js
+++ day_1_hw_js.js
@@ -86,8 +86,30 @@ var hedgehogIndex = animals["hedgehog"];
 var veg = ["swede", "turnip", "rutabega", "spinach", "carrot"];
 
 //6.2 Loop over the array and write to the console using a "while"
+
+counter = 0;
+while (counter < veg.length){
+  console.log("Vegetable is " + veg[counter]);
+  counter++;
+}
+
 //6.3 Loop again using a "for" with a counter
+
+for (i=0; i<veg.length;i++){
+  console.log("Vegetable is " + veg[i]);
+}
+
 //6.4 Loop again using a "for in"
+
+for (var vegetable in veg){
+  console.log("Vegetable is " + veg[vegetable]);
+}
+
+//6.5 Loop again using a "for of"
+
+for (var vegetable of veg){
+  console.log("Vegetable is " + vegetable);
+}
 
 //Section 7
 var accounts = [
--- test.js
+++ test.js
@@ -1,9 +1,21 @@
-function helloIfTrue(argument){
-     if (argument){
-          console.log("Hello");
-     } else {
-          console.log("Bye")
-     }
+var veg = ["swede", "turnip", "rutabega", "spinach", "carrot"];
+
+//6.2 Loop over the array and write to the console using a "while"
+
+counter = 0;
+while (counter < veg.length){
+  console.log("Vegetable is " + veg[counter]);
+  counter++;
 }
 
-helloIfTrue(false);
+//6.3 Loop again using a "for" with a counter
+
+for (i=0; i<veg.length;i++){
+  console.log("Vegetable is " + veg[i]);
+}
+
+//6.4 Loop again using a "for in"
+
+for (var vegetable in veg){
+  console.log("Vegetable is " + veg[vegetable]);
+}
